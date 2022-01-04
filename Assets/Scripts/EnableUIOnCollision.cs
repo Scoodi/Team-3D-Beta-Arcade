606a457e1e681919aedf6548bc231c57bf0f92cf
HAQUE <S6061420@live.tees.ac.uk>
2022-01-04 12:26:37 +0000

    enable/disable ui on collision programmatically

--- Assets/Scripts/EnableUIOnCollision.cs
+++ Assets/Scripts/EnableUIOnCollision.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseEnableUIOnCollision : MonoBehaviour
+public class EnableUIOnCollision : MonoBehaviour
 {
     public PlayerScript player;
-    protected List<GameObject> uiObjects = new List<GameObject>();
-    protected int UILayer = 5;
+    private List<GameObject> uiObjects = new List<GameObject>();
+    private int UILayer = 5;
+    public string Tag;
+    public UIScript playerHudCanvas;
 
     // Start is called before the first frame update
     protected void Start()
@@ -16,11 +18,29 @@ public class BaseEnableUIOnCollision : MonoBehaviour
         {
             obj.SetActive(false);
         }
+        player.UIEnabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            player.UIEnabled = true;
+            foreach (GameObject obj in uiObjects)
+            {
+                if (obj.tag == Tag)
+                {
+                    obj.SetActive(true);
+                }
+            }
+
+            player.ui = playerHudCanvas;
+        }
     }
 
     protected List<GameObject> findGameObjectsWithLayer(int layer)
     {
-        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
         List<GameObject> objectsWithLayer = new List<GameObject>();
 
         foreach (GameObject obj in allObjects)
