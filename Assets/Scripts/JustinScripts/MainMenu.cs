d113f59b6a4e99075097f9e0036f10ce28200ccf
Hikari2020 <w9168533@live.tees.ac.uk>
2022-01-05 10:46:55 +0000

    Save Point

--- Assets/Scripts/JustinScripts/MainMenu.cs
+++ Assets/Scripts/JustinScripts/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+    public void TutorialGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         Time.timeScale = 1f;
